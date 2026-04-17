b4e76d918f2a1137e21f1059523ae14000579265
Petrus Melo <158774367+petrusmelo@users.noreply.github.com>
2026-04-17 09:51:25 -0300

    Update Cálculo_do_Valor_Presente.py

--- Cálculo_do_Valor_Presente.py
+++ Cálculo_do_Valor_Presente.py
@@ -1,11 +1,11 @@
-def vp(vf,r,n):
-  x=vf/(1+r)**n
-  return(x)
+def vp(vf, r, n):
+    x = vf / (1 + r) ** n
+    return x
 
 valor_futuro=float(input("Informe o valor futuro: "))
 juros=float(input("Informe a taxa de juros: "))
+tempo=float(input("Informe o tempo: "))
 
-valor_presente=vp(valor_futuro,juros,1)
-
+valor_presente=vp(valor_futuro,juros,tempo)
 print('valor Futuro = ', valor_futuro )
 print('Valor Presente = ', valor_presente)
